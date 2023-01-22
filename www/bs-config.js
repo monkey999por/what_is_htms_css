5eec18b37850e696f08e06a66647dc973864f3b3
monkey999por <34706709+monkey999por@users.noreply.github.com>
2023-01-22 18:44:18 +0900

    add sass

--- www/bs-config.js
+++ www/bs-config.js
@@ -2,8 +2,8 @@ module.exports = {
   files: ["./main/**", ".main/images/*.jpg"], //監視するファイルを指定
   server: {
     //デフォルトで開くファイルを指定
-    baseDir: "./main/monkey999.work/scss_test", //ファイルまでのディレクトリのパス
-    index: "scss_test.html", //ファイル名
+    baseDir: "./main", //ファイルまでのディレクトリのパス
+    index: "index.html", //ファイル名
   },
   proxy: false,
   port: 8000, //使用するポート番号
